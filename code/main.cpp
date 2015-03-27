d4e0cd205505cfb4d60455a965671670f03bf41e
AlexanderPeev <peev.alexander@gmail.com>
2015-03-27 08:30:01 -0700

    Initial code from Eclipse.
    Signed-off-by: AlexanderPeev <peev.alexander@gmail.com>

--- code/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std; 
-
-int main(void)
-{
-  cout << "HELLO WORLD!";
-  return 0;
-}
---
+++ src/LifeForm.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : LifeForm.cpp
+// Author      : Alexander Peev
+// Version     :
+// Copyright   : Alexander Peev, 2015
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
